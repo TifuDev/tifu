2ddae57f99fbdc943242fbaabd7deae095e5a96e
Hytalo M. Bassi <hytalombassi@gmail.com>
2021-04-14 08:10:44 -0400

    Stylesheet back to normal and fixed meta description in layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,49 +1,12 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
-  purge: {
-    enabled: true,
-    content: ['./src/**/*.html'],
-    options: {
-      safelist: [
-        'flex',
-        'container',
-        /h[\d]/,
-        'p',
-        'span',
-        /text-(xs|sm|base|lg|xl|\dxl)/,
-        'p-2',
-        'm-2',
-        'list-disc',
-        'font-light',
-        'font-semibold',
-        'my-2',
-        'justify-between',
-        'items-center',
-        'shadow-2xl',
-        'mx-auto',
-        'w-full',
-        'flex-1',
-        'underline',
-        /grid/
-      ],
-    }
-  },
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      fontFamily: {
-        'sans': ['Open Sans']
-      }
-    },
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      blue: colors.blue
-    }
-  },
+  darkMode: 'media',
   variants: {
     extend: {},
   },
-  plugins: [],
+  purge: [
+    './public/views/*.pug',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  mode: 'jit',
+  plugins: []
 };
