049c6d0732a2e3b34799bdc34338d8f8e6ea8789
Hytalo M. Bassi <hytalombassi@gmail.com>
2021-05-26 15:38:04 -0400

    Beggining of docs. Fix methods to add Docs

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const express = require('express'),
     upload = require('./api/upload'),
     path = require('path'),
     user = require('./api/user'),
+    swagger = require('swagger-ui-express'),
     port = 3000;
 
 var app = express();
@@ -19,6 +20,160 @@ app.use(cookieParser());
 app.set('view engine', 'pug');
 app.set('views', 'public/views');
 app.use(express.static('public'));
+
+const docs = {
+    'openapi': '3.0.0',
+    'info': {
+        'version': '1.0.0',
+        'title': 'OpenAPI Docs'
+    },
+    'license': {
+        'name': 'Apache 2.0',
+        'url': 'https://www.apache.org/licenses/LICENSE-2.0.html'
+    },
+    'servers': [
+        {
+            'url': 'http://localhost:3000/api',
+            'description': 'Local Server'
+        }
+    ],
+    'tags': [
+        {
+            'name': 'News catalog'
+        },
+        {
+            'name': 'News'
+        }
+    ],
+    'paths': {
+        '/catalog':{
+            'get': {
+                'tags': ['News catalog'],
+                'description': 'Return JSON containing an array of news',
+                'operationId': 'getCatalog',
+                'responses': {
+                    '200': {
+                        'description': 'Catalog obtained',
+                        'content': {
+                            'aplication/json':{
+                                'schema': {
+                                    '$ref': '#/components/schema/Catalog'
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        '/new/{path}': {
+            'get': {
+                'tags': ['News'],
+                'description': 'Return New in JSON object',
+                'operationId': 'getNew',
+                'parameters': [{
+                    'in': 'path',
+                    'name': 'path',
+                    'required': true,
+                    'schema': {
+                        'type': 'string'
+                    },
+                    'description': 'Path for new'
+                }],
+                'responses':{
+                    '200': {
+                        'description': 'New obtained',
+                        'content': {
+                            'aplication/json': {
+                                'schema': {
+                                    '$ref': '#/components/schema/New'
+                                }
+                            }
+                        }
+                    },
+                    '404': {
+                        'description': 'The path informed can not be found'
+                    }
+                }
+            }
+        },
+        '/new/{path}/modify': {
+            'get': {
+                'tags': ['New'],
+                'description': 'Modify content of New',
+                'operationId': 'modifyNew',
+                'parameters': [{
+                    'in': 'path',
+                    'name': 'path',
+                    'required': true,
+                    'schema': {
+                        'type': 'string'
+                    },
+                    'description': 'Path for new'
+                }],
+                'security': [{
+                    'BearerAuth': ['admin']
+                }],
+                'responses': {
+                    '200': {
+                        'description': 'New modified',
+                        // ''
+                    }
+                }
+            }
+        }
+    },
+    'components': {
+        'securitySchemes': {
+            'BearerAuth': {
+                'type': 'http',
+                'scheme': 'bearer'
+            }
+        },
+        'schema': {
+            'Catalog': {
+                'type':'array'
+            },
+            'New': {
+                'type': 'object',
+                'properties': {
+                    'data': {
+                        'type': 'object',
+                        'properties': {
+                            '_id': {
+                                'type': 'integer'
+                            },
+                            'title': {
+                                'type': 'string'
+                            },
+                            'desc': {
+                                'type': 'string'
+                            },
+                            'path': {
+                                'type': 'string'
+                            },
+                            'author': {
+                                'type': 'string'
+                            },
+                            'date': {
+                                'type': 'string'
+                            },
+                            'downloads': {
+                                'type': 'integer'
+                            },
+                            'collectionId': {
+                                'type': 'integer'
+                            }
+                        }
+                    },
+                    'content': {
+                        'type': 'string'
+                    }
+                }
+            }
+        }
+    }
+};
+app.use('/api/docs', swagger.serve, swagger.setup(docs));
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -159,9 +314,7 @@ app.get('/api/new/:path/modify', sec.noticeOwner, (req, res) => {
     if(req.body.desc) req.notice.modifyNoticeDesc(req.body.desc);
     if(req.body.content) req.notice.modifyNoticeContent(req.body.content);
 
-    res.json({
-        message: 'Success'
-    });
+    res.sendStatus(204);
 });
 
 app.get('/api/upload/image', upload.handleBinary, (req, res) => {
