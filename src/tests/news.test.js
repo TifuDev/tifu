540e163aabe636e7ce0d838078bd7a17c505e9ba
hytalo-bassi <hytalo-bassi@protonmail.com>
2021-07-24 18:03:51 -0400

    Catalog added to tests and editorconfig added

--- src/tests/news.test.js
+++ src/tests/news.test.js
@@ -1,5 +1,5 @@
 const { mongoose } = require('../utils/db') 
-const { News } = require('../api/notice')
+const { News, seeCatalog } = require('../api/notice')
 
 const newObj = new News('abc') // Change the path parameter to your need
 const newNotFound = new News('some-randomic-path')
@@ -12,6 +12,10 @@ describe('Possible new responses', () => {
   test('Request to not existent new response', () => {
     return expect(newNotFound.get()).rejects.toStrictEqual(Error('New not found!'))
   })
+})
+
+test('Catalog response have more than 0 elements', () => {
+  return expect(seeCatalog()).resolves.not.toBe([])
 })
 
 afterAll(done => {
